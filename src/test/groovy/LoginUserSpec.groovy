3886c332b49cdcab2c2e113ff0b80e4e94344dc3
Dirk Günther <d.guenther@cenit.de>
2016-03-10 23:39:22 +0100

    Ersten Test implementiert

---
+++ src/test/groovy/LoginUserSpec.groovy
@@ -0,0 +1,76 @@
+import spock.lang.Specification
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+
+class LoginUserSpec extends Specification {
+
+	
+	def "User logon with correct username and password"() {
+		
+		given: "New RESTClient without login token"
+			def drtodolittle = new RESTClient( 'http://www.drtodolittle.de' )
+		
+		when: "Send username and password per POST Method"
+			def response = drtodolittle.post( path : '/api/todos/login',
+			contentType: JSON,
+			requestContentType:  JSON,
+			body: [email: "dirk", password: "dirk1234"])
+			
+		then: "Server send a valid JSON Web Token"
+		
+			response.status == 200
+			response.data.token.size() > 0
+	
+	}
+
+	
+	def "User logon with incorrect username and password"() {
+		
+		given: "New RESTClient without login token"
+			def drtodolittle = new RESTClient( 'http://www.drtodolittle.de' )
+		
+		when: "Send username and password per POST Method"
+			def response = drtodolittle.post( path : '/api/todos/login',
+			contentType: JSON,
+			requestContentType:  JSON,
+			body: [email: "dirk1", password: "dirk12345"])
+			
+		then: "Server send an unauthorized exception"
+			def e = thrown(groovyx.net.http.HttpResponseException)
+			e.getStatusCode() == 401
+	}
+	
+
+	def "User logon with empty username and password"() {
+		
+		given: "New RESTClient without login token"
+			def drtodolittle = new RESTClient( 'http://www.drtodolittle.de' )
+		
+		when: "Send username and password per POST Method"
+			def response = drtodolittle.post( path : '/api/todos/login',
+			contentType: JSON,
+			requestContentType:  JSON,
+			body: [email: "", password: ""])
+			
+		then: "Server send an unauthorized exception"
+			def e = thrown(groovyx.net.http.HttpResponseException)
+			e.getStatusCode() == 401
+	}
+
+	def "User logon with no username and password"() {
+		
+		given: "New RESTClient without login token"
+			def drtodolittle = new RESTClient( 'http://www.drtodolittle.de' )
+		
+		when: "Send username and password per POST Method"
+			def response = drtodolittle.post( path : '/api/todos/login',
+			contentType: JSON,
+			requestContentType:  JSON,
+			body: null)
+			
+		then: "Server send an unauthorized exception"
+			def e = thrown(groovyx.net.http.HttpResponseException)
+			e.getStatusCode() == 401
+	}
+	
+}
